c35b9928e0b8da4d9b0de81379c0aa281b24b864
Eleanor Atkinson <atkinson.el@northeastern.edu>
2023-12-10 22:58:38 -0500

    styling home

--- src/Project/Home/index.js
+++ src/Project/Home/index.js
@@ -6,6 +6,7 @@ import { MdDirectionsBike } from "react-icons/md";
 import "./index.css";
 import { FaGrinHearts } from "react-icons/fa";
 import { SlLike } from "react-icons/sl";
+import { FaHeart } from "react-icons/fa";
 
 function Home() {
   // State variables
@@ -41,7 +42,7 @@ function Home() {
   //     // Fetch the user's likes or perform other actions
   //   }
   // }, [authData]);
-  
+
 
   // useEffect to log the value of newestMember when it changes
   useEffect(() => {
@@ -57,42 +58,60 @@ function Home() {
       <br></br>
       {authData && authData.currentUser && (
         <div>
-        <h1> Welcome {authData.currentUser.username}, let's go for a ride. </h1>
+          <h1> Welcome {authData.currentUser.username}, let's go for a ride. </h1>
         </div>
       )}
-      <MdDirectionsBike className="bike-icon"/>
-      <MdDirectionsBike className="bike-icon-two"/>
-      <MdDirectionsBike className="bike-icon-three"/>
-      <MdDirectionsBike className="bike-icon-four"/>
-      <MdDirectionsBike className="bike-icon-five"/>
-      <div className="green-card-one">
-      <div className="title-text">Find bike stations in any city and share your favorites!</div>
-      <div className="button btn btn-light button-padding"> <Link to={`/search`} className="white-text-link">Click here to search for a city!</Link></div>
+      <MdDirectionsBike className="bike-icon" />
+      <MdDirectionsBike className="bike-icon-two" />
+      <MdDirectionsBike className="bike-icon-three" />
+      <MdDirectionsBike className="bike-icon-four" />
+      <MdDirectionsBike className="bike-icon-five" />
+      <div className="green-text-bubble">
+        <br></br>
+        <div className="">Find bike stations in any city... </div>
+        <div> Share your favorites! <FaHeart /></div>
+        <div className="styling-for-click">
+        <div className="button btn btn-light"> <Link to={`/search`} className="white-text-link">Click here to search for a city!</Link></div>
+        </div>
+        <br></br>
+        <br></br>
       </div>
       {newestMember && (
         <div>
-          <div className="background-color-card">
-          <div className={"float-end smile-icon"}> <FaGrinHearts /> </div>
-          <div className="title-text">Welcome Our Newest Member!</div>
-          <p>Their username is {newestMember.username}</p>
-          
-          <p>They joined on {new Date(newestMember.dateOfJoin).toLocaleDateString()}</p>
-          <p>
-            <Link className="link-text" to={`/profile/${newestMember._id}`}>View {newestMember.username}'s profile!</Link>
-          </p>
+          <div className="light-green-text-bubble">
+            <br></br>
+           
+            {/* <div className={"float-end smile-icon"}> <FaGrinHearts /> </div> */}
+            
+            <div className="list-group"> <b>Welcome Our Newest Member!</b>
+            
+            <hr></hr>
+            <p>
+              
+            Their username is {newestMember.username}</p>
+
+            <p>They joined on {new Date(newestMember.dateOfJoin).toLocaleDateString()}</p>
+            <p>
+              <Link className="link-text" to={`/profile/${newestMember._id}`}>View {newestMember.username}'s profile!</Link>
+            </p>
+            </div>
+            <br></br>
           </div>
           {/* Display the user's most recent like if available */}
           {authData && authData.currentUser && authData.userLikes.length > 0 && (
-            <div className="green-card-one">
-              <div className={"float-end thumbs-up-icon"}> <SlLike /> </div>
+            <div className="green-text-bubble">
+              <br></br>
+              {/* <div className={"float-end thumbs-up-icon"}> <SlLike /> </div> */}
+            
               <div className="title-text"> Your Most Recently Liked Station:</div>
+              <br></br>
               <p><b>{authData.currentUser.username}'s</b> most recently liked station is...
-              {authData.userLikes.sort((a, b) => new Date(b.dateOfLike) - new Date(a.dateOfLike))[0].station.name}
-              !  {/**/} </p>
+                {authData.userLikes.sort((a, b) => new Date(b.dateOfLike) - new Date(a.dateOfLike))[0].station.name}
+                !  {/**/} </p>
               <br></br>
               <br></br>
             </div>
-            
+
           )}
         </div>
       )}
